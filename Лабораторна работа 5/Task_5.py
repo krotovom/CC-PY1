311fbd6033a79e8aa573e1c9c3869b7e550cf1d8
Oleg <krotov.om@edu.spbstu.ru>
2022-11-14 10:37:54 +0300

    Update Task_5.py

--- Лабораторна работа 5/Task_5.py
+++ Лабораторна работа 5/Task_5.py
@@ -1,9 +1,9 @@
-def random_password():
+def random_password(x=8):
     import string
     random_ = list(string.ascii_letters)
     random_.extend(string.digits)
     from random import sample
-    random_pass = "".join(sample(random_, 8))
+    random_pass = "".join(sample(random_, x))
     return random_pass
 
 
