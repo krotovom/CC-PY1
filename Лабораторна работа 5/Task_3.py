edcbc6dfd4a2804d334556c8d2b9d74892c0eed9
Oleg <krotov.om@edu.spbstu.ru>
2022-11-14 16:17:19 +0300

    Update Task_3.py

--- Лабораторна работа 5/Task_3.py
+++ Лабораторна работа 5/Task_3.py
@@ -1,8 +1,10 @@
-def get_unique_list_numbers(x1=-10, x2=10, n=15):
-    import random
+import random
+
+
+def get_unique_list_numbers(left_edge=-10, right_edge=10, list_length=15):
     list_ = []
-    while len(list_) < n:
-        random_int = random.randint(x1, x2)
+    while len(list_) < list_length:
+        random_int = random.randint(left_edge, right_edge)
         list_.append(random_int)
         list_ = list(set(list_))
     return list_
